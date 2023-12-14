0a54514a71c958531263c2ac96e5c633b66f4454
smgKM984 <kozarova.julia@gmail.com>
2023-12-14 17:07:33 +0200

    Small correction

--- src/bg/smg/Main.java
+++ src/bg/smg/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	AnimalHouse<Pinguin> pinguinHouse=new AnimalHouse<>();
-        AnimalHouse<Bird> birdHouse=new AnimalHouse<>();
+     AnimalHouse<Bird> birdHouse=new AnimalHouse<>();
 
         Pinguin pinguin=new Pinguin("Kowalski");
         Bird bird=new Bird(12.0);
